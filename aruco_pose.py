3f9b2ec4d0f048b30c730f167589eab4d8df97b0
Alexander Mackworth <alexander@tssr.co>
2018-05-02 11:41:36 -0700

    position tracking via velocity integration

--- aruco_pose.py
+++ aruco_pose.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 import glob
+import time
 
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
 dictionary = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
@@ -27,7 +28,7 @@ for fname in images:
       allCharucoIds.append(res2[2])
 
     aruco.drawDetectedMarkers(gray, res[0], res[1])
-  
+
   cv2.imshow('frame', gray)
   cv2.waitKey(0)
 
@@ -37,39 +38,76 @@ mtx = cal[1]
 dist = cal[2]
 rvecs = cal[3][0]
 tvecs = cal[4][0]
-print cal
 
 cv2.destroyAllWindows()
 cap = cv2.VideoCapture(0)
+lastIDPosition = {}
+
+# goal: calculate position of viewer
+position = np.array([0, 0, 0])
+
 while(True):
-  # capture frames
-  ret, frame = cap.read()
+    # capture frames
+    start_time = time.time() # start time of the loop
 
-  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  parameters = aruco.DetectorParameters_create()
+    ret, frame = cap.read()
 
-  corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dictionary, parameters=parameters)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    parameters = aruco.DetectorParameters_create()
 
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dictionary, parameters=parameters)
 
-  if corners:
-    # print "id" + str(ids)
-    pose = aruco.estimatePoseSingleMarkers(corners, 0.1, mtx, dist, rvecs, tvecs)
-    # print pose
-    pose_rvecs = pose[0]
-    pose_tvecs = pose[1]
-    gray = aruco.drawDetectedMarkers(gray, corners)
-    for i in range(len(ids)):
-      aruco.drawAxis(gray, mtx, dist, pose_rvecs[i], pose_tvecs[i], 0.1)
+    if corners:
+        velocities = np.zeros((len(ids), 3))
 
-  # fix: creates a ton of windows
-  cv2.imshow('frame', gray)
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
+        pose = aruco.estimatePoseSingleMarkers(corners, 0.1, mtx, dist, rvecs, tvecs)
 
+        pose_rvecs = pose[0]
+        pose_tvecs = pose[1]
+        gray = aruco.drawDetectedMarkers(gray, corners)
+
+        for i in range(len(ids)):
+            # convert to rotation matrix
+            currentRotation, _ = cv2.Rodrigues(pose_rvecs[i])
+            # transpose and rotate coordinates to match the wall
+            currentTranslation = np.transpose(currentRotation.dot(np.transpose(pose_tvecs[i])))
+
+            aruco.drawAxis(gray, mtx, dist, pose_rvecs[i], currentTranslation, 0.1)
+
+            currentID = ids[i][0]
+            currentTime = time.time()
+
+            try:
+                (oldPosition, oldTime) = lastIDPosition[str(currentID)]
+
+                # filter out old entries, likely from last time the target crossed the camera
+                # number should be tuned more finely
+                if (currentTime - oldTime) > 5:
+                    lastIDPosition[str(currentID)] = (currentTranslation, currentTime)
+                    continue
+
+                # calculate velocity based on old position
+                velocity = (currentTranslation - oldPosition)/(currentTime - oldTime)
+                velocities[i] = velocity
+
+                lastIDPosition[str(currentID)] = (currentTranslation, currentTime)
+            except KeyError:
+                # first time seeing this target. remember for next time
+                lastIDPosition[str(currentID)] = (currentTranslation, currentTime)
+                continue
+
+        # integration...kinda
+        avgVelocity = np.average(velocities, axis=0) * 10
+        position = position + avgVelocity
+        print(position)
+
+    # fix: creates a ton of windows
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # print("FPS: ", 1.0 / (time.time() - start_time)) # FPS = 1 / time to process loop
 
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
